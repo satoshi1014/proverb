b311687bc9a3bf08e54e0367b3559b9ac32e78bb
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-17 08:53:28 +0900

    投稿フォーム完成

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,12 +1,9 @@
 class WordsController < ApplicationController
   def index
-    @proverb = Word.order("created_at DESC").page(params[:page]).per(6)
-  end
-
-  def new
+    @proverbs = Word.order("created_at DESC").page(params[:page]).per(6)
     @proverb = Word.new
   end
-
+  
   def create
     @proverb = Word.new(spot_params)
     if @proverb.save
