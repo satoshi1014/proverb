09933bb3ed5057cbca33a9704f7eedad691a5bb0
satoshi1014 <f.satoshi.1014@gmail.com>
2021-02-17 08:28:18 +0900

    newとcreateとバリデーションと部分テンプレートとルーティングをセッティング

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,19 @@
 class WordsController < ApplicationController
   def index
-    @spots = Spot.order("created_at DESC").page(params[:page]).per(6)
+    @proverb = Word.order("created_at DESC").page(params[:page]).per(6)
   end
+
+  def new
+    @proverb = Word.new
+  end
+
+  def create
+    @proverb = Word.new(spot_params)
+    if @proverb.save
+      redirect_to root_path
+    else
+      render action: :new
+    end 
+  end 
+
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,4 @@
 class Word < ApplicationRecord
+  validates :proverb, presence: true
+  validates :owner, presence: true
 end
